b782baf375584efb3658b33822af6708cfff54f9
jayramrout <jayram.rout@gmail.com>
2017-08-24 21:46:19 -0400

    Update

---
+++ HelloWorld/src/jrout/tutorial/servlet/HelloWorld.java
@@ -0,0 +1,59 @@
+package jrout.tutorial.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/helloWorldWithTime")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String course = request.getParameter("course");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Hello World...");
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<title>Hello World</title>");
+		out.print("</head>");
+		out.print("<body>");
+		
+		out.print("<h1>Hello "+name+" </h1>");
+		out.print("<h1>Welcome to the "+course +" Course</h1>");
+		out.print("<p>Now the time is "+new java.util.Date()+"</p>");
+
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
